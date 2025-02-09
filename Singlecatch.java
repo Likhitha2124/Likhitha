58e715b4bf40069d8fd9446a1ef9a9da1e758c02
Likhitha2124 <likhithamallepula1724@gmail.com>
2025-02-09 15:40:37 +0530

    Add files via upload

---
+++ Singlecatch.java
@@ -0,0 +1,17 @@
+public class Singlecatch{
+    public static void main(String[] args) {
+        int i = 0;
+        int j = 0;
+        try{
+            j = 18/i;
+
+        }
+        catch(Exception e){
+            System.out.println("something wen wrong");
+        }
+        System.out.println("j");
+        System.out.println("Bye");
+
+        
+    }
+}
